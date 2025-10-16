e6314632702f4ab35bb56754053c9da8d344ecb7
d3rpyd3rpd3rp <brandon.squires429@gmail.com>
2025-10-16 01:43:04 -0400

    Player now has the ability to shoot asteroids (currently does nothing)

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -21,6 +22,8 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -36,4 +39,7 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
-        
+    
+    def shoot(self):
+        velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        shot = Shot(self.position.x, self.position.y, velocity)
---
+++ shot.py
@@ -0,0 +1,14 @@
+import pygame
+from constants import SHOT_RADIUS
+from circleshape import CircleShape
+
+class Shot(CircleShape):
+    def __init__(self, x, y, velocity):
+        super().__init__(x, y, SHOT_RADIUS)
+        self.velocity = velocity
+        
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (self.position.x, self.position.y), SHOT_RADIUS)
+        
+    def update(self, dt):
+        self.position += self.velocity * dt
