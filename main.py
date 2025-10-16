2f0a89a244ef87741423b8127b24abf036a29bd7
d3rpyd3rpd3rp <brandon.squires429@gmail.com>
2025-10-16 00:18:09 -0400

    Delta time is now being used

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
@@ -13,6 +15,7 @@ def main():
         screen.fill((0,0,0))
         
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
     # print("Starting Asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}")
