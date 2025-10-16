1e7ec9f2a8ab83911f6a9f55380b2364acbb75ec
d3rpyd3rpd3rp <brandon.squires429@gmail.com>
2025-10-16 00:36:38 -0400

    Player now renders on screen

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
+        # input checks
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill((0,0,0))
+        # game update functions
         
+        
+        # game draw functions
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        # delta time update
         dt = clock.tick(60) / 1000
     
     # print("Starting Asteroids!")
