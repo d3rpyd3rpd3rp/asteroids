353bea1b05b7a4add51e06bd19bcc26dd72438f6
d3rpyd3rpd3rp <brandon.squires429@gmail.com>
2025-11-07 23:19:59 -0500

    Added logger and updated main to accommodate

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from shot import Shot
 def main():
     pygame.init()
     
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -49,10 +53,6 @@ def main():
         
         # delta time update
         dt = clock.tick(60) / 1000
-    
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
